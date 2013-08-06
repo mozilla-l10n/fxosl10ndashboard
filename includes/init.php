424520c788700f6a6031564328529e2d10831682
pascalc <pascal.chevrel@free.fr>
2013-08-06 13:12:50 +0200

    put cache folder back but commit it with maximum permissions, add a Debug parameter and a logging file since the server log file is not accessible to all users

--- includes/init.php
+++ includes/init.php
@@ -1,17 +1,17 @@
 <?php
-const INIT = true;
+const INIT  = true;
+const DEBUG = true;
 
 // Load local environment variables that depend on the hosting configuration
 require_once  __DIR__ .'/localenv.php';
 
 define('CACHE', $cachePath);
 
-ini_set("log_errors", 1);
-ini_set("error_log", "/tmp/fxoslocale-errors.log");
-error_log("---------------------------");
-
-if (!is_dir(CACHE)) {
-    mkdir(CACHE, 0777);
+// Set debug environment
+if(DEBUG) {
+    ini_set("log_errors", 1);
+    ini_set("error_log", "/tmp/fxoslocale-errors.log");
+    error_log("---------------------------");
 }
 
 // Include utility functions
