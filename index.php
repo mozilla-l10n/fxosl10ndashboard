9015a6c4d3afa4fe55b450effdd1a7cab0f4fe84
pascalc <pascal.chevrel@free.fr>
2013-08-06 09:27:30 +0200

    don't display the view twice

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ ob_start();
 include __DIR__ .'/views/overview.php';
 $content = ob_get_contents();
 ob_end_clean();
-print $content;
-
 
 // Show dashboard in template
 include __DIR__ .'/templates/base.tpl.php';
